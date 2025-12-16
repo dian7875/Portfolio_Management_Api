bd8c18c7bbc5fc75f85ba5b54e8f7164ef39056a
Dian <dian7875.a@gmail.com>
2025-12-15 20:26:59 -0600

    refactor: change auth module to global module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { envs } from 'src/config/envs.conf';
 
+@Global()
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
--- src/education/education.module.ts
+++ src/education/education.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EducationService } from './education.service';
 import { EducationController } from './education.controller';
-import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [EducationController],
-  providers: [EducationService],
-  imports:[AuthModule]
+  providers: [EducationService]
 })
 export class EducationModule {}
